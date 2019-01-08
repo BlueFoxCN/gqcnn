986cf1a79772c1b438f34ad9b753acb1a969ef7e
JesseYang <jesse.yang1985@gmail.com>
2019-01-08 15:58:46 +0800

    2019.01.08 15:58

--- reader.py
+++ reader.py
@@ -42,19 +42,20 @@ class Data(RNGDataFlow):
         with h5py.File(self.filename, 'r') as f:
             depth_im = f['depth_im'][idx]
             depth = f['hand_depth'][idx]
-            label = f['label'][idx]            
+            label = f['label'][idx]
         '''
-        
+
         num_1 = idx // 1000
         num_2 = idx % 1000
         depth_im = np.load(self.data_dir + 'depth_ims_tf_table_%05d.npz' % num_1)['arr_0'][num_2,...]
         depth = np.load(self.data_dir + 'hand_poses_%05d.npz' % num_1)['arr_0'][num_2,...][2]
-        angle = np.load(self.data_dir + 'hand_poses_%05d.npz' % num_1)['arr_0'][num_2,...][4]
+        angle = np.load(self.data_dir + 'hand_poses_%05d.npz' % num_1)['arr_0'][num_2,...][3]
+        depth = np.array([depth, angle])
         label = np.load(self.data_dir + 'robust_suction_wrench_resistance_%05d.npz' % num_1)['arr_0'][num_2,...]
         label = int(label > 0.2)
 
         add_noise = (not self.test_set) and np.random.rand() < cfg.gaussian_process_rate
-            
+
         if add_noise:
             gp_noise = ss.norm.rvs(scale=cfg.gp_sigma, size=cfg.noise_height * cfg.noise_width).reshape(cfg.noise_height, cfg.noise_width)
             gp_noise = misc.imresize(gp_noise, (cfg.im_height, cfg.im_width), interp='bicubic', mode='F')
@@ -64,7 +65,7 @@ class Data(RNGDataFlow):
         if self.save_data:
             misc.imsave(os.path.join(SAVE_DIR, '%d_%d_depth_im_%d.jpg' % (label, idx, int(add_noise))), depth_im[:,:,0])
 
-        return [depth_im, depth, angle, label]
+        return [depth_im, depth, label]
 
     def get_data(self):
         if not self.image_wise:
@@ -75,7 +76,7 @@ class Data(RNGDataFlow):
                 idxs = np.arange(cfg.train_num, cfg.tot_datapoints)
         else:
             if not self.test_set:
-                idxs = cfg.train_idxs 
+                idxs = cfg.train_idxs
                 self.rng.shuffle(idxs)
             else:
                 idxs = cfg.val_idxs
@@ -87,11 +88,11 @@ class Data(RNGDataFlow):
             yield retval
 
     def reset_state(self):
-        super(Data, self).reset_state() 
+        super(Data, self).reset_state()
 
 if __name__ == '__main__':
 
-    ds = Data(cfg.data_dir, save_data=True)       
+    ds = Data(cfg.data_dir, save_data=True)
     ds.reset_state()
 
     g = ds.get_data()
--- train.py
+++ train.py
@@ -45,7 +45,7 @@ class GQCNN(ModelDesc):
         return image
 
     def _get_inputs(self):
-        return [InputDesc(tf.float32, [None, cfg.im_height, cfg.im_width, 1], 'input'), 
+        return [InputDesc(tf.float32, [None, cfg.im_height, cfg.im_width, 1], 'input'),
                 InputDesc(tf.float32, [None], 'pose'),
                 InputDesc(tf.int32, [None], 'label')]
 
@@ -66,9 +66,9 @@ class GQCNN(ModelDesc):
                        .Conv2D('conv2_2', 64, 3, activation=LocalNorm)
                        # .LocalNorm('conv2_2_norm', cfg.radius, cfg.alpha, cfg.beta, cfg.bias)
                        .MaxPooling('pool2_2', 1)
-                       .FullyConnected('fc3', 1024)())      
+                       .FullyConnected('fc3', 1024)())
             if cfg.drop_fc3:
-                im_fc3 = tf.nn.dropout(fc3, cfg.fc3_drop_rate)     
+                im_fc3 = tf.nn.dropout(fc3, cfg.fc3_drop_rate)
             pc1 = FullyConnected('pc1', pose, 16)
 
         fc4_im = FullyConnected('fc4_im', im_fc3, 1024, activation=tf.identity)
@@ -76,7 +76,7 @@ class GQCNN(ModelDesc):
         fc4 = tf.nn.relu(fc4_im + fc4_pose)
         fc5 = FullyConnected('fc5', fc4, 2)
 
-        return fc5            
+        return fc5
 
     def _build_graph(self, inputs):
         image, pose, label = inputs
@@ -162,7 +162,7 @@ if __name__ == '__main__':
         os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 
     model = GQCNN()
-    
+
     if args.flops:
         input_desc = [
             InputDesc(tf.float32, [None, cfg.im_height, cfg.im_width, 1], 'input'),
